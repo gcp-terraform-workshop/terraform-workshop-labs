9a2238904a9326b18af106392af5f1a3b7b174d9
John Catalano <John.Catalano@insight.com>
2020-05-11 18:36:48 +0000

    Deleted solution/modules

--- 203-module-gcp/solution/modules/my_linux_vm/provider.tf
+++
@@ -1,8 +0,0 @@
-
-# variable "path" {default = "/home/udemy/terraform/credentials"}
-
-provider "google" {
-    project = "mimetic-pursuit-271713"
-    region = "us-east1-c"
-    # credentials = "${file("${var.path}/secrets.json")}"
-}
